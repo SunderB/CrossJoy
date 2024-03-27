a139eb42716a3bf81c270dac8eba1b949c6304aa
SunderB <sunder@sunderb.me>
2024-03-27 00:23:30 +0000

    Indentation: convert all tabs to spaces, and make indentation consistent

--- src/joycon.cpp
+++ src/joycon.cpp
@@ -10,9 +10,9 @@
 
 // Headers needed for sleeping.
 #ifdef _WIN32
-	#include <windows.h>
+  #include <windows.h>
 #else
-	#include <unistd.h>
+  #include <unistd.h>
 #endif
 
 template <typename T> std::string type_name();
@@ -26,73 +26,73 @@ JoyCon::JoyCon(uint jc_type, const char* serial_no) {
   unsigned char buf[256];
   unsigned char out_buf[256];
 
-	wchar_t wstr[255];
+  wchar_t wstr[255];
 
   type = jc_type;
   serial_number = std::string(serial_no);
 
   printf("Joycon\n vendor id: 0x%04hx\n product id: 0x%04hx\n", vendor_id, type);
 
-	// Set up the command buffer.
-	memset(buf,0x00,sizeof(buf));
-	buf[0] = 0x01;
-	buf[1] = 0x81;
+  // Set up the command buffer.
+  memset(buf,0x00,sizeof(buf));
+  buf[0] = 0x01;
+  buf[1] = 0x81;
 
 
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	////handle = hid_open(0x4d8, 0x3f, L"12345"serial_no.c_str());
-	handle = hid_open(vendor_id, type, str_to_wstr(serial_number).c_str());
-	if (!handle) {
+  // Open the device using the VID, PID,
+  // and optionally the Serial number.
+  ////handle = hid_open(0x4d8, 0x3f, L"12345"serial_no.c_str());
+  handle = hid_open(vendor_id, type, str_to_wstr(serial_number).c_str());
+  if (!handle) {
     std::wstring err_str;
     err_str = hid_error(NULL);
 
-		printf("unable to open device\n");
+    printf("unable to open device\n");
     printf("Error info: %ls \n", hid_error(NULL));
 
     if (err_str == L"Permission denied") {
       printf("(Try running this program using sudo)\n");
     }
 
- 		//return 1;
-	}
+     //return 1;
+  }
   printf("Handle: %8hhx\n", handle);
 
-	// Read the Manufacturer String
-	wstr[0] = 0x0000;
-	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read manufacturer string\n");
-	printf("Manufacturer String: %ls\n", wstr);
+  // Read the Manufacturer String
+  wstr[0] = 0x0000;
+  res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+  if (res < 0)
+    printf("Unable to read manufacturer string\n");
+  printf("Manufacturer String: %ls\n", wstr);
 
-	// Read the Product String
-	wstr[0] = 0x0000;
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read product string\n");
-	printf("Product String: %ls\n", wstr);
+  // Read the Product String
+  wstr[0] = 0x0000;
+  res = hid_get_product_string(handle, wstr, MAX_STR);
+  if (res < 0)
+    printf("Unable to read product string\n");
+  printf("Product String: %ls\n", wstr);
 
-	// Read the Serial Number String
-	wstr[0] = 0x0000;
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read serial number string\n");
-	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
-	printf("\n");
+  // Read the Serial Number String
+  wstr[0] = 0x0000;
+  res = hid_get_serial_number_string(handle, wstr, MAX_STR);
+  if (res < 0)
+    printf("Unable to read serial number string\n");
+  printf("Serial Number String: (%d) %ls", wstr[0], wstr);
+  printf("\n");
 
-	// Read Indexed String 1
-	//wstr[0] = 0x0000;
-	//res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	//if (res < 0)
-	//	printf("Unable to read indexed string 1\n");
-	//printf("Indexed String 1: %ls\n", wstr);
+  // Read Indexed String 1
+  //wstr[0] = 0x0000;
+  //res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
+  //if (res < 0)
+  //	printf("Unable to read indexed string 1\n");
+  //printf("Indexed String 1: %ls\n", wstr);
 
-	// Set the hid_read() function to be non-blocking.
-	hid_set_nonblocking(handle, 1);
+  // Set the hid_read() function to be non-blocking.
+  hid_set_nonblocking(handle, 1);
 
-	// Try to read from the device. There should be no
-	// data here, but execution should not block.
-	res = hid_read(handle, buf, 17);
+  // Try to read from the device. There should be no
+  // data here, but execution should not block.
+  res = hid_read(handle, buf, 17);
 
   // Make sure command is received
   //hid_set_nonblocking(handle, 0);
@@ -167,71 +167,71 @@ JoyCon::JoyCon(uint jc_type, const char* serial_no) {
     }
   }
 
-	// Send a Feature Report to the device
-	//buf[0] = 0x2;
-	//buf[1] = 0xa0;
-	//buf[2] = 0x0a;
-	//buf[3] = 0x00;
-	//buf[4] = 0x00;
-	//res = hid_send_feature_report(handle, buf, 17);
-	//if (res < 0) {
-	//	printf("Unable to send a feature report.\n");
-	//}
+  // Send a Feature Report to the device
+  //buf[0] = 0x2;
+  //buf[1] = 0xa0;
+  //buf[2] = 0x0a;
+  //buf[3] = 0x00;
+  //buf[4] = 0x00;
+  //res = hid_send_feature_report(handle, buf, 17);
+  //if (res < 0) {
+  //	printf("Unable to send a feature report.\n");
+  //}
 
-	// memset(buf,0,sizeof(buf));
+  // memset(buf,0,sizeof(buf));
   //
-	// // Read a Feature Report from the device
-	// buf[0] = 0x2;
-	// res = hid_get_feature_report(handle, buf, sizeof(buf));
-	// if (res < 0) {
-	// 	printf("Unable to get a feature report.\n");
-	// 	printf("%ls", hid_error(handle));
-	// }
-	// else {
-	// 	// Print out the returned buffer.
-	// 	printf("Feature Report\n   ");
-	// 	for (i = 0; i < res; i++)
-	// 		printf("%02hhx ", buf[i]);
-	// 	printf("\n");
-	// }
+  // // Read a Feature Report from the device
+  // buf[0] = 0x2;
+  // res = hid_get_feature_report(handle, buf, sizeof(buf));
+  // if (res < 0) {
+  // 	printf("Unable to get a feature report.\n");
+  // 	printf("%ls", hid_error(handle));
+  // }
+  // else {
+  // 	// Print out the returned buffer.
+  // 	printf("Feature Report\n   ");
+  // 	for (i = 0; i < res; i++)
+  // 		printf("%02hhx ", buf[i]);
+  // 	printf("\n");
+  // }
 
-	//memset(buf,0,sizeof(buf));
+  //memset(buf,0,sizeof(buf));
 
-	// Toggle LED (cmd 0x80). The first byte is the report number (0x1).
-	//buf[0] = 0x1;
-	//buf[1] = 0x80;
-	//res = hid_write(handle, buf, 17);
-	//if (res < 0) {
-	//	printf("Unable to write()\n");
-	//	printf("Error: %ls\n", hid_error(handle));
-	//}
-
-
-	// Request state (cmd 0x81). The first byte is the report number (0x1).
+  // Toggle LED (cmd 0x80). The first byte is the report number (0x1).
+  //buf[0] = 0x1;
+  //buf[1] = 0x80;
+  //res = hid_write(handle, buf, 17);
+  //if (res < 0) {
+  //	printf("Unable to write()\n");
+  //	printf("Error: %ls\n", hid_error(handle));
+  //}
 
 
-	// Read requested state. hid_read() has been set to be
-	// non-blocking by the call to hid_set_nonblocking() above.
-	// This loop demonstrates the non-blocking nature of hid_read().
-	// res = 0;
-	// while (res == 0) {
-	// 	res = hid_read(handle, buf, sizeof(buf));
-	// 	if (res == 0)
-	// 		printf("waiting...\n");
-	// 	if (res < 0)
-	// 		printf("Unable to read()\n");
-	// 	#ifdef WIN32
-	// 	Sleep(500);
-	// 	#else
-	// 	usleep(500*1000);
-	// 	#endif
-	// }
+  // Request state (cmd 0x81). The first byte is the report number (0x1).
+
+
+  // Read requested state. hid_read() has been set to be
+  // non-blocking by the call to hid_set_nonblocking() above.
+  // This loop demonstrates the non-blocking nature of hid_read().
+  // res = 0;
+  // while (res == 0) {
+  // 	res = hid_read(handle, buf, sizeof(buf));
+  // 	if (res == 0)
+  // 		printf("waiting...\n");
+  // 	if (res < 0)
+  // 		printf("Unable to read()\n");
+  // 	#ifdef WIN32
+  // 	Sleep(500);
+  // 	#else
+  // 	usleep(500*1000);
+  // 	#endif
+  // }
   //
-	// printf("Data read:\n   ");
-	// // Print out the returned buffer.
-	// for (i = 0; i < res; i++)
-	// 	printf("%02hhx ", buf[i]);
-	// printf("\n");
+  // printf("Data read:\n   ");
+  // // Print out the returned buffer.
+  // for (i = 0; i < res; i++)
+  // 	printf("%02hhx ", buf[i]);
+  // printf("\n");
 }
 
 void JoyCon::subcommand(unsigned char sc, unsigned char buf[], uint len, unsigned char response[]) {
--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,9 @@
 
 // Headers needed for sleeping.
 #ifdef _WIN32
-	#include <windows.h>
+  #include <windows.h>
 #else
-	#include <unistd.h>
+  #include <unistd.h>
 #endif
 
 #include "hidapi/hidapi.h"
@@ -34,43 +34,43 @@ template <typename T> std::string type_name();
 
 int main(int argc, char* argv[])
 {
-	(void)argc;
-	(void)argv;
+  (void)argc;
+  (void)argv;
 
-	int res;
+  int res;
 
   unsigned char buf[256];
   unsigned char out_buf[256];
 
   #define MAX_STR 255
-	wchar_t wstr[MAX_STR];
-	int i;
+  wchar_t wstr[MAX_STR];
+  int i;
 
-	struct hid_device_info *devs, *cur_dev;
+  struct hid_device_info *devs, *cur_dev;
 
-	printf("JoyCon hidapi test/example tool. Compiled with hidapi version %s, runtime version %s.\n", HID_API_VERSION_STR, hid_version_str());
-	if (hid_version()->major == HID_API_VERSION_MAJOR && hid_version()->minor == HID_API_VERSION_MINOR && hid_version()->patch == HID_API_VERSION_PATCH) {
-		printf("Compile-time version matches runtime version of hidapi.\n\n");
-	}
-	else {
-		printf("Compile-time version is different than runtime version of hidapi.\n]n");
-	}
-
-	if (hid_init()) {
-    printf("Failed to initialise hidapi.");
-		return -1;
+  printf("JoyCon hidapi test/example tool. Compiled with hidapi version %s, runtime version %s.\n", HID_API_VERSION_STR, hid_version_str());
+  if (hid_version()->major == HID_API_VERSION_MAJOR && hid_version()->minor == HID_API_VERSION_MINOR && hid_version()->patch == HID_API_VERSION_PATCH) {
+    printf("Compile-time version matches runtime version of hidapi.\n\n");
+  }
+  else {
+    printf("Compile-time version is different than runtime version of hidapi.\n]n");
   }
 
-	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;
-	uint32_t joycon_vid = 0;
-	uint32_t joycon_pid = 0;
+  if (hid_init()) {
+    printf("Failed to initialise hidapi.");
+    return -1;
+  }
+
+  devs = hid_enumerate(0x0, 0x0);
+  cur_dev = devs;
+  uint32_t joycon_vid = 0;
+  uint32_t joycon_pid = 0;
   std::map<std::wstring, JoyCon*> joycons;
   std::vector<std::wstring> joycon_serial_nos;
 
-	while (cur_dev) {
-		//std::cout << cur_dev->path)>() << '\n';
-		//if (std::string(cur_dev->vendor_id).compare("/dev/hidraw1") == 0) {
+  while (cur_dev) {
+    //std::cout << cur_dev->path)>() << '\n';
+    //if (std::string(cur_dev->vendor_id).compare("/dev/hidraw1") == 0) {
     if (cur_dev->vendor_id == 0x057e) {
       std::wstring serial_no = std::wstring(cur_dev->serial_number);
       switch (cur_dev->product_id) {
@@ -101,21 +101,21 @@ int main(int argc, char* argv[])
           printf("Not a Joycon");
           break;
       }
-    	//joycon_vid = cur_dev->vendor_id;
-			//joycon_pid = cur_dev->product_id;
+      //joycon_vid = cur_dev->vendor_id;
+      //joycon_pid = cur_dev->product_id;
 
-		}
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("  Release:      %hx\n", cur_dev->release_number);
-		printf("  Interface:    %d\n",  cur_dev->interface_number);
-		printf("  Usage (page): 0x%hx (0x%hx)\n", cur_dev->usage, cur_dev->usage_page);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
+    }
+    printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+    printf("\n");
+    printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+    printf("  Product:      %ls\n", cur_dev->product_string);
+    printf("  Release:      %hx\n", cur_dev->release_number);
+    printf("  Interface:    %d\n",  cur_dev->interface_number);
+    printf("  Usage (page): 0x%hx (0x%hx)\n", cur_dev->usage, cur_dev->usage_page);
+    printf("\n");
+    cur_dev = cur_dev->next;
+  }
+  hid_free_enumeration(devs);
   //printf("Joycon\n vendor id: 0x%04hx\n product id: 0x%04hx\n", joycon_vid, joycon_pid);
 
   // for (size_t i = 0; i < joycon_serial_nos.size(); i++) {
@@ -128,12 +128,12 @@ int main(int argc, char* argv[])
   // }
 
 
-	/* Free static HIDAPI objects. */
-	hid_exit();
+  /* Free static HIDAPI objects. */
+  hid_exit();
 
 #ifdef WIN32
-	system("pause");
+  system("pause");
 #endif
 
-	return 0;
+  return 0;
 }
